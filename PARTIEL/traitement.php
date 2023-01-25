f8a1aae1a662f4ebf66721cd1960e71b4cf9ce6b
Adrien Tandabany <tandabany.adrien@hotmail.fr>
2023-01-25 23:26:45 +0100

    Modification variables developpeur et manager

--- PARTIEL/traitement.php
+++ PARTIEL/traitement.php
@@ -2,11 +2,11 @@
 
 require "ressources.php";
 
-$developpeur = array(new Developpeur(550,"Le Flock", "Louis-Marie", "lmleflock@test.net",
-"0606060606",2500, "Java"),
-new Developpeur(650,"Poirie", "Ethan", "ach@test.net", "0707070707",2750, "C++"));
+$developpeur = array(new Developpeur(550,"Le Flock", "lmleflock@test.net", "0606060606",2500, "Java"),
+new Developpeur(650,"Chellal", "ach@test.net", "0707070707",2750, "C++"),
+new Developpeur(151,"Katsiev", "amir@test.net", "0606060606",2750, "Kotlin"));
  
-$manager = array() ;
+$manager = array(new Manager(500,"RIPAULT", "Alexis", "alexis@manager.net", "0606060606",4000, "DSI")) ;
 
 
 foreach ($developpeur as $value) {
